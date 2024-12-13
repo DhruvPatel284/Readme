4715370f38ab9138bb7be4276ca6ebe3559291c0
DhruvPatel284 <dhruv156328@gmail.com>
2024-12-13 16:27:08 +0530

    some minor mistake fixed

--- Readme-main/frontend/src/config.ts
+++ Readme-main/frontend/src/config.ts
@@ -1,5 +1,5 @@
-//export const BACKEND_URL = "https://backend.dhruv156328.workers.dev"
-export const BACKEND_URL = "http://127.0.0.1:8787"
+export const BACKEND_URL = "https://backend.dhruv156328.workers.dev"
+//export const BACKEND_URL = "http://127.0.0.1:8787"
 // export const BACKEND_URL = "http://localhost:5000"
 export const RAG_URL="https://rag.dhruv156328.workers.dev"
 //export const RAG_URL="http://127.0.0.1:8787"
